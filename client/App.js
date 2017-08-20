c3505b0358529a182dea71d2f93dd196920edeaf
albertadevor <alberta.devor@gmail.com>
2017-08-20 15:42:44 +0100

    Added async asset loading for fonts and images

--- client/App.js
+++ client/App.js
@@ -1,29 +1,70 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import styles from './SharedStyles.js';
+import { AppLoading, Asset, Font } from 'expo';
 
 export default class App extends React.Component {
-
 
  constructor() {
     super()
     this.state = { 
-      loggedIn: false
+      loggedIn: false,
+      assetsAreLoaded: false,
     }
     //TODO: this is being currently mocked out 
   }
 
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    try {
+      await Promise.all([
+        Asset.loadAsync([
+          require('./assets/img/old_comp.png') //TODO add other images
+        ]),
+        Font.loadAsync([
+          // This is the font that we are using for our tab bar
+          //Ionicons.font,
+          // We include SpaceMono because we use it in HomeScreen.js. Feel free
+          // to remove this if you are not using it in your app
+          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+        ]),
+      ]);
+    } catch (e) {
+      // In this case, you might want to report the error to your error
+      // reporting service, for example Sentry
+      console.warn(
+        'There was an error caching assets (see: App.js), perhaps due to a ' +
+          'network timeout, so we skipped caching. Reload the app to try again.'
+      );
+      console.log(e);
+    } finally {
+      this.setState({ assetsAreLoaded: true });
+    }
+  }
+
   render() {
-    console.log("hi my state is" + this.state.loggedIn)
+    if (!this.state.assetsAreLoaded) {
+      //TODO make app loading screen and have app loading logic
+
+      //return <AppLoading />;
+      return (<Text> Still loading </Text>);
+    }
     if (!this.state.loggedIn) {
-      console.log("logged in!")
       return (
         <View style={styles.container}>
-          <Text>"We're not logged in yet haha!"</Text>
-          <Image
-          style={{width: 50, height: 50}}
-          source={{uri: 'img/'}}/>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <View style={styles.contentContainer}>
+            <Text style ={styles.h1}>Courseler</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Image
+            style={classStyles.computer_logo}
+            source={require('./assets/img/old_comp.png')}/>
+            <Text>Changes you make will automatically reload.</Text>
+            <Text>Shake your phone to open the developer menu.</Text>
+          </View>
         </View>
       );
       // return <LoggedInView onLogOut={() => {this.setState({loggedIn: false})}} route={this.state.route} />;
@@ -34,11 +75,11 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const classStyles = StyleSheet.create({
+  computer_logo: {
+    flex: 2,
+    maxHeight: 200,
+    borderColor:'red',
+    resizeMode: 'contain',
   },
 });
---
+++ client/SharedStyles.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    alignItems: 'center',
+  },
+  h1: {
+    fontSize: 40,
+    fontFamily: 'space-mono',
+    color: '#333',
+    paddingBottom: 20,
+  }
+});
