544d9649883489dce948027fe001dfd60f45b63b
albertadevor <alberta.devor@gmail.com>
2017-08-22 23:20:33 +0100

    Set up login screen with the fake API

---
+++ client/api/API.js
@@ -0,0 +1,3 @@
+import { API } from './FakeAPI.js';
+let api = new API();
+export default api;
--- client/screens/LetsLogInPage.js
+++ client/screens/LetsLogInPage.js
@@ -5,8 +5,8 @@ import LoadingView from 'react-native-loading-view';
 import { Button } from 'react-native-elements';
 import t from 'tcomb-form-native'
 import FloatingLabel from 'react-native-floating-label'
+import api from '../api/API.js'
 var dismissKeyboard = require('dismissKeyboard');
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const Form = t.form.Form
 const Login = t.struct({
@@ -24,12 +24,14 @@ export default class LetsLogInPage extends React.Component {
 		this.state = {
 			assetsAreLoading: true,
 			loginScroll: null,
+			email: "",
+			password: "",
+			error: "",
 			value: {},
 			options: {
 		        fields: {
 		          email: {
 		            factory: FloatingLabel,
-		            onFocus: this.formFocused,
 		          },
 		          password: {
 		            factory: FloatingLabel,
@@ -39,17 +41,38 @@ export default class LetsLogInPage extends React.Component {
 		}
 	}
 
-	// Scroll form into view.
-	formFocused() {
-	  // 	setTimeout(() => {
-	  // 	console.log(this.refs)
-	  //   let scrollResponder = scrollView.getScrollResponder();
-	  //   scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
-	  //   React.findNodeHandle(loginForm),
-	  //     110, //additionalOffset
-	  //     true
-	  //   );
-	  // }, 50);
+	handleLoginResult(result) {
+		console.log(result)
+		if (result.status === 'success') {
+	        this.props.onLogIn();
+	    } else if (result.status === 'unregistered') {
+	        this.setState({error: "There's no account with that email address!"});
+	    } else if (result.status === 'already_registered') {
+	        this.setState({error: "There's already an account with that email address!"});
+	    } else if (result.status === 'wrong_password') {
+	        this.setState({error: "Incorrect password."});
+	    } else {
+	        this.setState({error: "Unknown error :("});
+	    }
+	};
+
+	redirectToSignup() {
+		//TODO
+	}
+
+	sendLoginRequest() {
+		var formVal = this.refs.form.getValue();
+	    if(formVal && formVal.email && formVal.password) {
+	    	api.logIn(formVal.email, formVal.password, (results) => {
+      		this.handleLoginResult(results)
+    		});
+	    }
+	}
+
+
+
+	update() {
+
 	}
 
 	// componentDidMount() {
@@ -57,21 +80,44 @@ export default class LetsLogInPage extends React.Component {
 	// 	loginForm = this.refs.form;
 	// }
 
+		// <KeyboardAwareScrollView
+		// 		contentContainerStyle = {classStyles.vertical}
+		// 		getTextInputRefs={() => { return [this._emptyRef];}}
+		// 		scrollEnabled={false}>
+
+		   // <View ref={(r) => { this._textInputRef = r; }} />
+
 	render() {
+
+		let error = <Text>{this.state.error}</Text>
 		return(
 			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-				<KeyboardAwareScrollView
-				contentContainerStyle = {classStyles.vertical}
-				scrollEnabled={false}
-				resetScrollToCoords={{ x: 0, y: 0 }}>
+				<View style = {classStyles.vertical}>
 					<Text style = {styles.h1}> Log In </Text>
+					{error}
 					<View style = {classStyles.container}>
-						<Form ref='form'
-				          	type={Login}
+						<Form type={Login}
+							ref='form'
 				          	value={this.state.value}
 				          	options={this.state.options} />
+				          	<View style = {classStyles.buttonContainer}>
+					        	<Button
+								raised
+								buttonStyle={{backgroundColor: 'rgba(89,92,188,1)', 
+								borderRadius: 10, width: 150}}
+								textStyle={{textAlign: 'center'}}
+								onPress={() => this.sendLoginRequest()}
+								title={`Log In`}/>
+
+								<Button buttonStyle={{backgroundColor: 'white', 
+								width: 300}}
+								textStyle={{textAlign: 'center', color: '#111'}}
+								onPress={() => this.redirectToSignup()}
+								title={`Or click here to sign up Instead`}/> 
+							</View>
+							
 				    </View>
-				</KeyboardAwareScrollView>
+				</View>
 			</TouchableWithoutFeedback>
 		);
 	}
@@ -83,13 +129,15 @@ const classStyles = StyleSheet.create({
 		fontSize: 40,
 		fontFamily: 'space-mono',
 		color: '#333',
-		paddingBottom: -20,
 	},
 	container: {
 	    justifyContent: 'center',
+	    alignContent: 'center',
 	    marginTop: 50,
 	    padding: 20,
 	    width: 300,
+	    marginBottom: 130,
+	    marginTop: -30,
 	},
 	vertical: {
 		flex: 1,
@@ -97,5 +145,9 @@ const classStyles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-
+	buttonContainer: {
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+	}
 });
